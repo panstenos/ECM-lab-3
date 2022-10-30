89d6ec0bf0e8212e77f2c589aaedea68e0a60b7e
panstenos <112823396+panstenos@users.noreply.github.com>
2022-10-30 20:37:43 +0000

    Exercise 2.2

--- main.c
+++ main.c
@@ -12,13 +12,15 @@
 #include "timers.h"
 
 #define _XTAL_FREQ 64000000 //note intrinsic _delay function is 62.5ns at 64,000,000Hz  
-
+#include <stdio.h>
 
 void main(void) {
-	//call timer and LED initialisation functions to set up the  modules
+	//call comparator and interrupts initialisation functions to set up the hardware modules
+    LEDarray_init();    
     Timer0_init();
-    LEDarray_init();
+    //Comp1_init();
+    //Interrupts_init();
     while (1) {
-        LEDarray_disp_bin(TMR0L); //use the LED to display the timer
+        LEDarray_disp_bin(get16bitTMR0val() >> 8); //use the LED to display the timer
     }
 }
