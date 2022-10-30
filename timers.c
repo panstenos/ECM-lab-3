2e5bb20352f8ff474bc62f57163f25ee8d7de087
panstenos <112823396+panstenos@users.noreply.github.com>
2022-10-30 20:39:43 +0000

    Exercise 2.2
    change from 8 bit to 16 bit timer
    complete the get16bitTMR0val(void) function

--- timers.c
+++ timers.c
@@ -9,10 +9,10 @@ void Timer0_init(void)
     T0CON1bits.T0CS=0b010; // Fosc/4
     T0CON1bits.T0ASYNC=1; // see datasheet errata - needed to ensure correct operation when Fosc/4 used as clock source
     T0CON1bits.T0CKPS=0b1111; // 1:32768
-    T0CON0bits.T016BIT=0;	//8bit mode	
+    T0CON0bits.T016BIT=1;	//8bit mode	
 	
     // it's a good idea to initialise the timer registers so we know we are at 0
-    TMR0H=0;            //write High reg first, update happens when low reg is written to
+    //TMR0H=0;            //write High reg first, update happens when low reg is written to
     TMR0L=0;
     T0CON0bits.T0EN=1;	//start the timer
 }
@@ -23,5 +23,6 @@ void Timer0_init(void)
 ************************************/
 unsigned int get16bitTMR0val(void)
 {
-	//add your code to get the full 16 bit timer value here
+	return TMR0L | (TMR0H << 8); //return the 8 MSB
+
 }
