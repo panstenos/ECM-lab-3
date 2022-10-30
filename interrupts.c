cd7daad765be277a1bba3ca6d66554cbc7fde453
panstenos <112823396+panstenos@users.noreply.github.com>
2022-10-30 17:26:26 +0000

    Exercise 1.2

--- interrupts.c
+++ interrupts.c
@@ -6,9 +6,9 @@
  * Note you also need to enable peripheral interrupts in the INTCON register to use CM1IE.
 ************************************/
 void Interrupts_init(void)
-{
-	// turn on global interrupts, peripheral interrupts and the interrupt source 
-	// It's a good idea to turn on global interrupts last, once all other interrupt configuration is done.
+{   INTCONbits.PEIE = 1;    //turns on all the peripheral interrupts
+    PIE2bits.C1IE = 1;      //enable the comparator interrupt bit
+    INTCONbits.GIE = 1; 	//turn on interrupts globally (when this is off, all interrupts are deactivated)
 }
 
 /************************************
@@ -16,7 +16,10 @@ void Interrupts_init(void)
  * Make sure all enabled interrupts are checked and flags cleared
 ************************************/
 void __interrupt(high_priority) HighISR()
-{
-	//add your ISR code here i.e. check the flag, do something (i.e. toggle an LED), clear the flag...
+{   
+    if(PIR2bits.C1IF == 1){//check the interrupt source
+        LATHbits.LATH3 = !LATHbits.LATH3; //toggle LED
+        PIR2bits.C1IF = 0; // turn flag off
+	}
 }
 
