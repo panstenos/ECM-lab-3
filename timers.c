23c36c2147136d34200dc212dffaf8c7d429594b
panstenos <112823396+panstenos@users.noreply.github.com>
2022-10-31 17:23:35 +0000

    Exercise 2.4
    Corrections from 2.2 -> set TMR0H=0

--- timers.c
+++ timers.c
@@ -10,9 +10,9 @@ void Timer0_init(void)
     T0CON1bits.T0ASYNC=1; // see datasheet errata - needed to ensure correct operation when Fosc/4 used as clock source
     T0CON1bits.T0CKPS=0b1000; // 1:256 -> required: 1:244.14
     T0CON0bits.T016BIT=1;	//8bit mode	
-	
+    
     // it's a good idea to initialise the timer registers so we know we are at 0
-    //TMR0H=0;            //write High reg first, update happens when low reg is written to
+    TMR0H=0;            //write High reg first, update happens when low reg is written to
     TMR0L=0;
     T0CON0bits.T0EN=1;	//start the timer
 }
