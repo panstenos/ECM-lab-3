04e800bbf795ad00d6225500be5004e9bc8d771e
panstenos <112823396+panstenos@users.noreply.github.com>
2022-10-31 18:00:52 +0000

    Exercise 2.5

--- timers.c
+++ timers.c
@@ -9,11 +9,11 @@ void Timer0_init(void)
     T0CON1bits.T0CS=0b010; // Fosc/4
     T0CON1bits.T0ASYNC=1; // see datasheet errata - needed to ensure correct operation when Fosc/4 used as clock source
     T0CON1bits.T0CKPS=0b1000; // 1:256 -> required: 1:244.14
-    T0CON0bits.T016BIT=1;	//8bit mode	
+    T0CON0bits.T016BIT=1;//8bit mode	
     
-    // it's a good idea to initialise the timer registers so we know we are at 0
-    TMR0H=0;            //write High reg first, update happens when low reg is written to
-    TMR0L=0;
+    // initialise the time registers to 3036 for the LED to toggle every second
+    TMR0H=0b00001011;            
+    TMR0L=0b11011011;
     T0CON0bits.T0EN=1;	//start the timer
 }
 
