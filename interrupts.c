ff955e18c0ba423c2f58a2c3b66cbce6a5b184f8
panstenos <112823396+panstenos@users.noreply.github.com>
2022-10-31 17:25:49 +0000

    Exercise 2.4
    Change the HighISR function

--- interrupts.c
+++ interrupts.c
@@ -6,9 +6,11 @@
  * Note you also need to enable peripheral interrupts in the INTCON register to use CM1IE.
 ************************************/
 void Interrupts_init(void)
-{   INTCONbits.PEIE = 1;    //turns on all the peripheral interrupts
-    PIE2bits.C1IE = 1;      //enable the comparator interrupt bit
-    INTCONbits.GIE = 1; 	//turn on interrupts globally (when this is off, all interrupts are deactivated)
+{   
+    INTCONbits.PEIE = 1;    //turns on all the peripheral interrupts
+    PIE0bits.TMR0IE = 1;    // enable Timer0 interrupt
+    PIE2bits.C1IE = 1;    //enable the comparator interrupt bit
+    INTCONbits.GIE = 1; 	//turn on interrupts globally (when this is off, all interrupts are deactivated)    
 }
 
 /************************************
@@ -17,9 +19,9 @@ void Interrupts_init(void)
 ************************************/
 void __interrupt(high_priority) HighISR()
 {   
-    if(PIR2bits.C1IF == 1){//check the interrupt source
+    if(PIR0bits.TMR0IF == 1){//check the interrupt source
         LATHbits.LATH3 = !LATHbits.LATH3; //toggle LED
-        PIR2bits.C1IF = 0; // turn flag off
+        PIR0bits.TMR0IF = 0; // turn flag off
 	}
 }
 
